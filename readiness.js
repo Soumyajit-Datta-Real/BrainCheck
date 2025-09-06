4642e4b3d1e0ee58aed272d7ad7385155d523835
Soumyajit Dutta <75132388+Soumyajit-Datta-Real@users.noreply.github.com>
2025-09-06 11:35:12 +0530

    Delete readiness.js

--- readiness.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("readinessForm").addEventListener("submit", function (e) {
-  // You can add client-side validation if needed
-  console.log("Submitting readiness survey...");
-});
